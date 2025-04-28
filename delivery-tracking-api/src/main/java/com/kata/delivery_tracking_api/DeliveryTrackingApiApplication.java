a163737c9bc47a84039fa03f71846a0331513eeb
mohammedtrb <mohammedtourab@gmail.com>
2025-04-28 14:13:32 +0100

    init project

---
+++ delivery-tracking-api/src/main/java/com/kata/delivery_tracking_api/DeliveryTrackingApiApplication.java
@@ -0,0 +1,13 @@
+package com.kata.delivery_tracking_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliveryTrackingApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliveryTrackingApiApplication.class, args);
+	}
+
+}
