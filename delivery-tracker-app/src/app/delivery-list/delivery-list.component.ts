be51d422b04e4e592b2c710f79ed04f836ca24c4
mohammedtrb <mohammedtourab@gmail.com>
2025-04-29 16:07:23 +0100

    Iimplement delivery loading functionalities

--- delivery-tracker-app/src/app/delivery-list/delivery-list.component.ts
+++ delivery-tracker-app/src/app/delivery-list/delivery-list.component.ts
@@ -1,4 +1,7 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { DeliveryService } from '../services/delivery.service';
+import { Router } from '@angular/router';
 
 interface Delivery {
   id: number;
@@ -19,7 +22,7 @@ export enum DeliveryState {
 
 @Component({
   selector: 'app-delivery-list',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './delivery-list.component.html',
   styleUrl: './delivery-list.component.css'
 })
@@ -27,6 +30,28 @@ export class DeliveryListComponent {
   customerId = 1; //TODO get it later from auth service 
   deliveries: Delivery[] = [];
   errorMessage = '';
+  
+  constructor(private deliveryService: DeliveryService, private router: Router) { }
 
-  ngOnInit(): void { }
+  ngOnInit(): void {
+    this.loadDeliveries();
+  }
+
+  loadDeliveries(): void {
+    this.deliveryService.getCustomerDeliveries(this.customerId).subscribe({
+      next: (deliveries) => {
+        this.deliveries = deliveries;
+      },
+      error: (error) => {
+        this.errorMessage = 'Failed to load deliveries.';
+        console.error('Error loading deliveries:', error);
+      }
+    });
+  }
+
+  viewDetails(deliveryId: number): void {
+
+    console.log('View details for delivery ID:', deliveryId);
+    this.router.navigate(['/deliveries', deliveryId]);
+  }
 }
