2e6d9596298db0090329b828d3d98374cb90a226
mohammedtrb <mohammedtourab@gmail.com>
2025-04-29 16:08:29 +0100

    Add routing

--- delivery-tracker-app/src/app/app.routes.ts
+++ delivery-tracker-app/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { DeliveryListComponent } from './delivery-list/delivery-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'deliveries', component: DeliveryListComponent },    
+    { path: '', redirectTo: '/deliveries', pathMatch: 'full' }
+];
